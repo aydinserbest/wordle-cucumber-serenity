0a9211b44b92e8fb630117d135fa8c92effd8597
aydinSerbest <aydinserbest34@gmail.com>
2024-04-28 17:58:50 +0200

    updated

---
+++ src/test/java/serenitydojo/WordleStepDefinitions.java
@@ -0,0 +1,21 @@
+package serenitydojo;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WordleStepDefinitions {
+    @Given("the target word is :")
+    public void the_target_word_is(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+    @When("the player enters the following letters:")
+    public void the_player_enters_the_following_letters(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+    @Then("the squares should be coloredas follows:")
+    public void the_squares_should_be_coloredas_follows(io.cucumber.datatable.DataTable dataTable) {
+
+    }
+
+}
