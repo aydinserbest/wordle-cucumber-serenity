023ce48bd28c6fa5c1997de2f062dc5985654cf1
aydinSerbest <aydinserbest34@gmail.com>
2024-04-30 01:07:18 +0200

    updated

--- src/test/java/serenitydojo/WordleStepDefinitions.java
+++ src/test/java/serenitydojo/WordleStepDefinitions.java
@@ -14,33 +14,40 @@ import java.util.stream.Stream;
 
 
 public class WordleStepDefinitions {
+    String target;
+    List<String> attempts;
+    List<List<CellColor>> renderedRows;
+
     @Given("the target word is :")
-    public void the_target_word_is(DataTable dataTable) {
-        List<List<String>> lists = dataTable.asLists(String.class); // asLists() kullanarak dönüştürme
+    public void the_target_word_is(DataTable targetWord) {
+        this.target = String.join("", targetWord.asLists().get(0));
 
-//        List<String> list = lists.stream()
-//                .flatMap(Collection::stream)
-//                .toList();
-
-        // OR
-        List<String> flattenedList = new ArrayList<>();
-        for (List<String> list : lists) {
-            flattenedList.addAll(list);
-        }
-        String targetWord = String.join("", flattenedList); // Tek bir kelime oluştur
-
-       // String targetWord = String.join("", list); // Tek bir kelime oluştur
-
-        System.out.println(targetWord);
+        System.out.println(target);
 
     }
     @When("the player enters the following letters:")
-    public void the_player_enters_the_following_letters(io.cucumber.datatable.DataTable dataTable) {
+    public void the_player_enters_the_following_letters(DataTable dataTable) {
+        List<String> attempts2 = dataTable.asLists()
+                .stream()
+                .map(cellsInRow-> String.join("", cellsInRow))
+                .toList();
+        System.out.println(attempts2);
 
     }
-    @Then("the squares should be coloredas follows:")
-    public void the_squares_should_be_coloredas_follows(io.cucumber.datatable.DataTable dataTable) {
+    @Then("the squares should be colored as follows:")
+    public void the_squares_should_be_coloredas_follows(DataTable dataTable) {
+        this.renderedRows = dataTable.asLists()
+                .stream()
+                .map(listOfCellSymbols -> colored(listOfCellSymbols))
+                .collect(Collectors.toList());
 
+        System.out.println(renderedRows);
+
+    }
+    private List<CellColor> colored(List<String> listOfCellSymbols) {
+        return listOfCellSymbols.stream()
+                .map(symbol -> CellColor.withSymbol(symbol))
+                .collect(Collectors.toList());
     }
 
 }
